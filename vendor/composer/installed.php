8b1f83bd6dea21763ea101844af0e2a8f2b88bf8
jose alfredo ponce <joseponce001@gmail.com>
2022-07-21 04:08:50 -0500

    test

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '41db60ae47afcf3c55b0914481a0d2c66cba54ab',
         'name' => 'codeigniter/framework',
         'dev' => true,
     ),
@@ -20,12 +20,12 @@
             'dev_requirement' => true,
         ),
         'codeigniter/framework' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '41db60ae47afcf3c55b0914481a0d2c66cba54ab',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
